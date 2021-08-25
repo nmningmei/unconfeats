64756446045fbba471965d2f507d723df00dadfe
nmningmei <semsociaty@gmail.com>
2021-08-25 16:28:18 +0800

    delete unncessary folders

---
+++ scripts/simulation/0.1.binary_cross_entropy_loss.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 25 16:22:43 2021
+
+@author: ning
+"""
+
+import torch
+
+from matplotlib import pyplot as plt
+
+predictions = torch.linspace(0,1)
