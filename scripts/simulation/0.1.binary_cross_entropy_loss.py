289bde1613d66b8c9367d5c2791b696c710bf6dd
nmningmei <semsociaty@gmail.com>
2021-08-25 16:41:51 +0800

    add README.md

--- scripts/simulation/0.1.binary_cross_entropy_loss.py
+++ scripts/simulation/0.1.binary_cross_entropy_loss.py
@@ -10,4 +10,25 @@ import torch
 
 from matplotlib import pyplot as plt
 
-predictions = torch.linspace(0,1)
+n_points = 1000
+n_gaps = 250
+loss_function = torch.nn.BCELoss()
+predictions = torch.linspace(0,1,n_points)
+ground_true = torch.ones(n_points) * 0.5
+
+losses = [loss_function(y_pred,y_true).item() for y_pred,y_true in zip(predictions,ground_true)]
+x = predictions.numpy()
+
+fig,axes = plt.subplots(figsize = (10,5),
+                        ncols = 2,
+                        sharey = False,
+                        sharex = False,
+                        )
+ax = axes[0]
+ax.plot(x,losses)
+ax.set(xlabel = 'Prediction',
+       ylabel = 'Loss')
+ax = axes[1]
+ax.plot(x[1:-1],losses[1:-1])
+ax.set(xlabel = 'Prediction',
+       ylabel = 'Loss')
